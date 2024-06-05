e59cdb7e6b7c2a56a019b52da300b35277eee296
Nikita-Vesel0v <nikiveselo2004@gmail.com>
2024-06-05 18:07:26 +0300

    program can search substring in several strings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
 package search
 
 fun main() {
-    val words = readln().split(" ")
-    val wordIndex = words.indexOf(readln())
-    println(if (wordIndex != -1) wordIndex + 1; else "Not found")
+    println("Enter the number of people:")
+    val size = readln().toInt()
+
+    println("Enter all people:")
+    val db = List(size) { readln() }
+
+    println("\nEnter the number of search queries:")
+    val numberQuery = readln().toInt()
+
+    var wordFind: String
+    var found: Boolean
+    val resultSearch = mutableListOf<String>()
+
+    for (i in 1..numberQuery) {
+        println("\nEnter data to search people:")
+        wordFind = readln().lowercase()
+        found = false
+        db.forEach { if (it.lowercase().contains(wordFind)) { resultSearch.add(it); found = true } }
+
+        println(if(!found) {
+            "No matching people found."
+        } else {
+            "\nPeople found:\n${resultSearch.joinToString(separator = "\n")}"
+        })
+        resultSearch.clear()
+    }
 }
+
